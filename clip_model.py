c66a92afb7ee2764a23032a03fc62d21ff1a1e5b
Ishan Shah <ishan0102@utexas.edu>
2023-03-27 13:23:19 -0700

    Fix CLIP model by running on CPU

--- clip_model.py
+++ clip_model.py
@@ -1,32 +1,40 @@
-from typing import Dict, List
-
 import sieve
 
 
 @sieve.Model(
     name="clip",
     gpu=False,
-    python_packages=[
-        "git+https://github.com/openai/CLIP.git",
+    python_packages=["torch==1.8.1", "git+https://github.com/openai/CLIP.git"],
+    run_commands=[
+        "mkdir -p /root/.cache/clip",
+        "wget -O /root/.cache/clip/ViT-B-32.pt https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
     ],
-    python_version="3.9",
-    persist_output=True,
+    iterator_input=True,
 )
 class Clip:
     def __setup__(self):
-        import torch
         import clip
 
-        self.clip_device = "cpu"
-        self.clip_model, self.clip_preprocess = clip.load("ViT-B/32", device=self.clip_device)
+        self.model, self.preprocess = clip.load("ViT-B/32", device="cpu")
 
-    def __predict__(self, img: sieve.Image) -> List:
-        import torch
+    def __predict__(self, images: sieve.Image):
         from PIL import Image
 
-        image = self.clip_preprocess(Image.open(img.path)).unsqueeze(0).to(self.clip_device)
-
-        with torch.no_grad():
-            image_features = self.clip_model.encode_image(image)
-
-        yield image_features
+        for image in images:
+            metadata = {
+                "id": image.id,
+                "type": image.type,
+                "video_name": image.video_name,
+                "frame_number": image.frame_number,
+                "x0": image.x0,
+                "y0": image.y0,
+                "x1": image.x1,
+                "y1": image.y1,
+                "video_path": image.video_path,
+            }
+            preprocessed_image = self.preprocess(Image.open(image.path)).unsqueeze(0).to("cpu")
+            image_features = self.model.encode_image(preprocessed_image)
+            features = list(image_features.cpu().detach().numpy()[0])
+            features = [float(x) for x in features]
+            print(type(features), type(features[0]), len(features))
+            yield {"features": features, "metadata": metadata}
--- search.py
+++ search.py
@@ -8,7 +8,7 @@ from splitter import VideoSplitter
 
 
 @sieve.workflow(name="copilot_search")
-def copilot_search(video: sieve.Video) -> list:
-    images = VideoSplitter(video)
+def copilot_search(video: sieve.Video, name: str) -> dict:
+    images = VideoSplitter(video, name)
     clip_outputs = Clip()(images)
     return clip_outputs
