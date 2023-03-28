90733c173d4001c09525075b5213e473b9fe86dd
Ishan Shah <ishan0102@utexas.edu>
2023-03-28 16:18:05 -0700

    Move Pinecone and CLIP into models

---
+++ embeddings.py
@@ -0,0 +1,158 @@
+from typing import Dict, List
+
+import sieve
+
+
+@sieve.Model(
+    name="pinecone-upload-images",
+    python_packages=[
+        "pinecone-client==2.2.1",
+        "python-dotenv==0.21.1",
+        "torch==1.8.1",
+        "git+https://github.com/openai/CLIP.git",
+    ],
+    run_commands=[
+        "mkdir -p /root/.cache/clip",
+        "wget -O /root/.cache/clip/ViT-B-32.pt https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
+    ],
+    iterator_input=True,
+)
+class PineconeUploadImages:
+    def __setup__(self):
+        import clip
+
+        self.model, self.preprocess = clip.load("ViT-B/32", device="cpu")
+
+    def __predict__(self, images: sieve.Image, user_id: str):
+        import os
+
+        import pinecone
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+        from PIL import Image
+
+        clip_embeddings = []
+        for image in images:
+            metadata = {
+                "type": image.type,
+                "video_name": image.video_name,
+                "frame_number": image.frame_number,
+                "frame_count": image.frame_count,
+                "fps": image.fps,
+                "x0": image.x0,
+                "y0": image.y0,
+                "x1": image.x1,
+                "y1": image.y1,
+                "video_path": image.video_path,
+            }
+
+            preprocessed_image = self.preprocess(Image.open(image.path)).unsqueeze(0).to("cpu")
+            image_features = self.model.encode_image(preprocessed_image)
+            features = list(image_features.cpu().detach().numpy()[0])
+            features = [float(x) for x in features]
+            clip_embeddings.append({"id": image.id, "features": features, "metadata": metadata})
+
+        api_key = os.getenv("PINECONE_API_KEY")
+        pinecone.init(api_key=api_key, environment="us-west1-gcp")
+        index_name = "video-copilot"
+        index = pinecone.Index(index_name=index_name)
+
+        vectors = []
+        for clip_embedding in clip_embeddings:
+            vectors.append((clip_embedding["id"], clip_embedding["features"], clip_embedding["metadata"]))
+
+        # HACK: convert iterator input to string
+        for user in user_id:
+            user_id = user
+            break
+
+        response = index.upsert(vectors=vectors, namespace=user_id)
+        if "upserted_count" in response:
+            print(f"Successfully upserted {response['upserted_count']} vectors")
+        else:
+            print("Failed to upsert vectors")
+
+        return clip_embeddings
+
+
+@sieve.Model(
+    name="pinecone-query-text",
+    python_packages=[
+        "pinecone-client==2.2.1",
+        "python-dotenv==0.21.1",
+        "torch==1.8.1",
+        "git+https://github.com/openai/CLIP.git",
+    ],
+    run_commands=[
+        "mkdir -p /root/.cache/clip",
+        "wget -O /root/.cache/clip/ViT-B-32.pt https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt",
+    ],
+    iterator_input=True,
+)
+class PineconeQueryText:
+    def __setup__(self):
+        import clip
+
+        self.model, self.preprocess = clip.load("ViT-B/32", device="cpu")
+
+    def __predict__(self, commands: Dict, user_id: str) -> Dict:
+        import os
+
+        import clip
+        import pinecone
+        from dotenv import load_dotenv
+
+        load_dotenv()
+
+        api_key = os.getenv("PINECONE_API_KEY")
+        pinecone.init(api_key=api_key, environment="us-west1-gcp")
+        index_name = "video-copilot"
+        index = pinecone.Index(index_name=index_name)
+
+        # HACK: convert iterator input to string
+        for user in user_id:
+            user_id = user
+            break
+
+        for command in commands:
+            if "query" not in command:
+                print(f"Skipping {command} (no query)")
+                yield command
+                continue
+
+            print(f"Querying for {command['query']}")
+            query = command["query"]
+            for t in query:
+                tokenized = clip.tokenize([t]).to("cpu")
+                text_features = self.model.encode_text(tokenized)
+                features = list(text_features.cpu().detach().numpy()[0])
+                features = [float(x) for x in features]
+
+            matches = index.query(
+                vector=features,
+                top_k=5,
+                namespace=user_id,
+                include_metadata=True,
+            )["matches"]
+
+            min_frame = float("inf")
+            max_frame = float("-inf")
+
+            print(f"Nearest neighbors: {matches}")
+            for match in matches:
+                frame_number = match["metadata"]["frame_number"]
+                if frame_number < min_frame:
+                    min_frame = frame_number
+                if frame_number > max_frame:
+                    max_frame = frame_number
+
+            video_name = matches[0]["metadata"]["video_name"]
+            fps = matches[0]["metadata"]["fps"]
+
+            command["id"] = video_name
+            command["start"] = round(min_frame / fps, 2)
+            command["end"] = round(max_frame / fps, 2)
+            del command["query"]
+            yield command
--- upload.py
+++ upload.py
@@ -5,50 +5,12 @@ from typing import Dict, List
 
 import sieve
 
-from clip_image import ClipImageEncoder
+from embeddings import PineconeUploadImages
 from splitter import video_splitter
-
-
-@sieve.function(
-    name="pinecone-upload",
-    python_packages=[
-        "pinecone-client==2.2.1",
-        "python-dotenv==0.21.1",
-    ],
-    iterator_input=True,
-)
-def pinecone_upload(clip_embeddings: List, user_id: str):
-    import os
-
-    import pinecone
-    from dotenv import load_dotenv
-
-    load_dotenv()
-
-    api_key = os.getenv("PINECONE_API_KEY")
-    pinecone.init(api_key=api_key, environment="us-west1-gcp")
-    index_name = "video-copilot"
-    index = pinecone.Index(index_name=index_name)
-
-    vectors = []
-    for clip_embedding in clip_embeddings:
-        vectors.append((clip_embedding["id"], clip_embedding["features"], clip_embedding["metadata"]))
-
-    # HACK: convert iterator input to string
-    for user in user_id:
-        user_id = user
-        break
-
-    response = index.upsert(vectors=vectors, namespace=user_id)
-    if "upserted_count" in response:
-        print(f"Successfully upserted {response['upserted_count']} vectors")
-    else:
-        print("Failed to upsert vectors")
 
 
 @sieve.workflow(name="copilot_upload")
 def copilot_upload(video: sieve.Video, name: str, user_id: str) -> Dict:
     images = video_splitter(video, name)
-    clip_outputs = ClipImageEncoder()(images)
-    response = pinecone_upload(clip_outputs, user_id)
-    return clip_outputs
+    response = PineconeUploadImages()(images, user_id)
+    return response
